f58d80d84116291f3c5aa48650faf6b15ecf74ad
Ilya <malygin.ilja@yandex.ru>
2022-01-16 19:38:57 +0300

    Добавил возможность вызывать пользовательские функции(контроллер)

--- src/app.php
+++ src/app.php
@@ -1,9 +1,34 @@
 <?php
+//use Symfony\Component\Routing;
+////Маршруты url
+//$routes = new Routing\RouteCollection();
+////массив значений по умолчанию атрибута Routing\Route array('name' => 'World')
+//$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
+//$routes->add('bye', new Routing\Route('/bye'));
+//
+//return $routes;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
-//Маршруты url
+
+function is_leap_year($year = null)
+{
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
 $routes = new Routing\RouteCollection();
-//массив значений по умолчанию атрибута Routing\Route array('name' => 'World')
-$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Routing\Route('/bye'));
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Это високосный год!');
+        }
+        return new Response('Нет, это не весокосный год');
+    }
+]));
+
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -5,6 +5,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function renderTemplate($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $response = new Response();
 $routes = include __DIR__.'/../src/app.php';
@@ -20,14 +29,13 @@ try {
     //Cовпадающий маршрут автоматически хранится в специальном атрибуте _route
     //Импортирует переменные из массива в текущую таблицу символов
     //EXTR_SKIP = Если переменная с таким именем существует, её текущее значение не будет перезаписано
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
 
-    $response = new Response(ob_get_clean());
-} catch (Routing\Exception\ResourceNotFoundException $e) {
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    //Вызывается пользовательская функция из app.php через ключ-"_controller", далее передаются аргументы запроса
+    $response = call_user_func($request->attributes->get('_controller'), $request);
+} catch (Routing\Exception\ResourceNotFoundException $exeption) {
     $response = new Response('Файл не найден', 404);
-} catch (Exception $e) {
+} catch (Exception $exeption) {
     $response = new Response('Ошибка сервера', 500);
 }
 

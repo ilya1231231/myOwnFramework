0baed5cabd3506b9d0df6b1ebf10084aa87e1f37
Ilya <malygin.ilja@yandex.ru>
2022-01-24 19:39:22 +0300

    Добавил слушателя обработки ошибок

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -11,55 +11,56 @@ use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\HttpKernel\HttpKernel;
 
 
 //Повтроно используемый код фреймворка, который абстрагирует обработку входящих Запросов
 
-class Framework implements HttpKernelInterface
+class Framework extends HttpKernel
 {
-    private $dispatcher;
-    protected $matcher;
-    protected $controllerResolver;
-    protected $argumentResolver;
-
-    public function __construct(
-        EventDispatcher $dispatcher,
-        UrlMatcherInterface $matcher,
-        ControllerResolverInterface $controllerResolver,
-        ArgumentResolverInterface $argumentResolver)
-    {
-        $this->dispatcher = $dispatcher;
-        $this->matcher = $matcher;
-        $this->controllerResolver = $controllerResolver;
-        $this->argumentResolver = $argumentResolver;
-    }
-
-    //Кеширование
-    public function handle(Request $request, int $type = self::MAIN_REQUEST, bool $catch = true): Response
-    {
-        $this->matcher->getContext()->fromRequest($request);
-
-        try {
-            //Метод match() класса UrlMatcher берёт путь запроса и возвращает массив атрибутов
-            //Cовпадающий маршрут автоматически хранится в специальном атрибуте _route
-            //Импортирует переменные из массива в текущую таблицу символов
-            //EXTR_SKIP = Если переменная с таким именем существует, её текущее значение не будет перезаписано
-            $request->attributes->add($this->matcher->match($request->getPathInfo()));
-
-            $controller = $this->controllerResolver->getController($request);
-            $arguments = $this->argumentResolver->getArguments($request, $controller);
-
-            return call_user_func_array($controller, $arguments);
-        } catch (ResourceNotFoundException $e) {
-            $response = new Response('Файл не найден', 404);
-        } catch (\Exception $e) {
-            $response =  new Response('Ошибка сервера', 500);
-        }
-
-        // развернуть событие ответа
-        $this->dispatcher->dispatch(new ResponseEvent($response, $request), 'response');
-
-        return $response;
-
-    }
+//    private $dispatcher;
+//    protected $matcher;
+//    protected $controllerResolver;
+//    protected $argumentResolver;
+//
+//    public function __construct(
+//        EventDispatcher $dispatcher,
+//        UrlMatcherInterface $matcher,
+//        ControllerResolverInterface $controllerResolver,
+//        ArgumentResolverInterface $argumentResolver)
+//    {
+//        $this->dispatcher = $dispatcher;
+//        $this->matcher = $matcher;
+//        $this->controllerResolver = $controllerResolver;
+//        $this->argumentResolver = $argumentResolver;
+//    }
+//
+//    //Кеширование
+//    public function handle(Request $request, int $type = self::MAIN_REQUEST, bool $catch = true): Response
+//    {
+//        $this->matcher->getContext()->fromRequest($request);
+//
+//        try {
+//            //Метод match() класса UrlMatcher берёт путь запроса и возвращает массив атрибутов
+//            //Cовпадающий маршрут автоматически хранится в специальном атрибуте _route
+//            //Импортирует переменные из массива в текущую таблицу символов
+//            //EXTR_SKIP = Если переменная с таким именем существует, её текущее значение не будет перезаписано
+//            $request->attributes->add($this->matcher->match($request->getPathInfo()));
+//
+//            $controller = $this->controllerResolver->getController($request);
+//            $arguments = $this->argumentResolver->getArguments($request, $controller);
+//
+//            return call_user_func_array($controller, $arguments);
+//        } catch (ResourceNotFoundException $e) {
+//            $response = new Response('Файл не найден', 404);
+//        } catch (\Exception $e) {
+//            $response =  new Response('Ошибка сервера', 500);
+//        }
+//
+//        // развернуть событие ответа
+//        $this->dispatcher->dispatch(new ResponseEvent($response, $request), 'response');
+//
+//        return $response;
+//
+//    }
 }
--- web/front.php
+++ web/front.php
@@ -1,35 +1,75 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 
-//ФРОНТ КОНТРОЛЛЕР
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\Routing;
+
 $request = Request::createFromGlobals();
+$requestStack = new RequestStack();
 $routes = include __DIR__.'/../src/app.php';
 
-//Для кастомной обработки событий при запросах с приоритетами
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
-$dispatcher->addSubscriber(new Simplex\GoogleListener());
-
 $context = new Routing\RequestContext();
-//Основываясь на информации, хранящейся в экземпляре "src/app.php" RouteCollection, экземпляр UrlMatcher может совпадать с путями URL
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 $controllerResolver = new HttpKernel\Controller\ControllerResolver();
 $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
 
-//не забыть использовать Composer dump autoload
-//переместили все в отдельный класс
-$framework = new Simplex\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
+//Добавление listner'a кастомных ошибок
+$listener = new HttpKernel\EventListener\ErrorListener(
+    'Calendar\Controller\ErrorController::exception'
+);
+
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
+$dispatcher->addSubscriber($listener);
+$dispatcher->addSubscriber(new Simplex\StringResponseListener());
+
+$framework = new Simplex\Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+
 $framework = new HttpKernel\HttpCache\HttpCache(
     $framework,
     new HttpKernel\HttpCache\Store(__DIR__.'/../cache')
 );
-$response = $framework->handle($request);
-$framework->handle($request)->send();
 
+$response = $framework->handle($request);
 $response->send();
+
+//require_once __DIR__.'/../vendor/autoload.php';
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Routing;
+//use Symfony\Component\HttpKernel;
+//use Symfony\Component\EventDispatcher\EventDispatcher;
+//
+////ФРОНТ КОНТРОЛЛЕР
+//$request = Request::createFromGlobals();
+//$routes = include __DIR__.'/../src/app.php';
+//
+////Для кастомной обработки событий при запросах с приоритетами
+//$dispatcher = new EventDispatcher();
+//$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
+//$dispatcher->addSubscriber(new Simplex\GoogleListener());
+//
+//$context = new Routing\RequestContext();
+////Основываясь на информации, хранящейся в экземпляре "src/app.php" RouteCollection, экземпляр UrlMatcher может совпадать с путями URL
+//$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+//
+//$controllerResolver = new HttpKernel\Controller\ControllerResolver();
+//$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+//
+////не забыть использовать Composer dump autoload
+////переместили все в отдельный класс
+//$framework = new Simplex\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
+//$framework = new HttpKernel\HttpCache\HttpCache(
+//    $framework,
+//    new HttpKernel\HttpCache\Store(__DIR__.'/../cache')
+//);
+//$response = $framework->handle($request);
+//$framework->handle($request)->send();
+//
+//$response->send();
